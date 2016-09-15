5cb1c7e1d13d3c7b60616e83713475e99c380155
Miron <mironsss@icloud.com>
2016-09-15 18:53:27 +0300

    first push

---
+++ app/src/main/java/com/calculator/miron/testwork_03/MainActivity.java
@@ -0,0 +1,24 @@
+package com.calculator.miron.testwork_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+
+    }
+}
